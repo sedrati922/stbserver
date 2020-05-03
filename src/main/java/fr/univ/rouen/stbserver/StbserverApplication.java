5998e45d59534c13c0e896b3f75a29a7350b4af9
sedrati salah eddine <sedrati922@gmail.com>
2020-05-03 19:52:52 +0200

    premier commit

---
+++ src/main/java/fr/univ/rouen/stbserver/StbserverApplication.java
@@ -0,0 +1,13 @@
+package fr.univ.rouen.stbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StbserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StbserverApplication.class, args);
+	}
+
+}
